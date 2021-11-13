67a8621aebd0a49d1488d14053c8dc4aa41d6ee9
Peter Delis <delispeter19@gmail.com>
2021-11-13 15:10:33 -0500

    Add main code

---
+++ arduino/pong_lift_code/pong_lift_code.ino
@@ -0,0 +1,85 @@
+#include <Servo.h>
+
+Servo servo;
+//Serial port;
+
+int incomingByte = 0;
+const int A1A = 3;
+const int A1B = 2;
+const int B1A = 4;
+const int B1B = 5;
+
+void motorA(char c){
+  if(c == 'R'){
+    digitalWrite(A1A,LOW);
+    digitalWrite(A1B,HIGH);
+  } else if(c == 'L'){
+    digitalWrite(A1A,HIGH);
+    digitalWrite(A1B,LOW);
+  } else {
+    digitalWrite(A1A,LOW);
+    digitalWrite(A1B,LOW);
+  }
+}
+
+
+void motorB(char c){
+  if(c == 'R'){
+    digitalWrite(B1A,LOW);
+    digitalWrite(B1B,HIGH);
+  } else if(c == 'L'){
+    digitalWrite(B1A,HIGH);
+    digitalWrite(B1B,LOW);
+  } else {
+    digitalWrite(B1A,LOW);
+    digitalWrite(B1B,LOW);
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  servo.attach(9);
+  pinMode(A1A, OUTPUT);
+  pinMode(A1B, OUTPUT);
+  pinMode(B1A, OUTPUT);
+  pinMode(B1B, OUTPUT);
+//  port = new Serial(this, Serial.list()[0], 9600);
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+    // ARM UP -- W
+    if(incomingByte == 119){
+      servo.write(90);
+    }
+    // ARM DOWN -- S
+    if(incomingByte == 115){
+      servo.write(5);
+    }
+    
+    // Forward -- T
+    if(incomingByte == 116){
+      motorA('R');
+      motorB('R');
+    }
+    // Backward -- G
+    if(incomingByte == 103){
+      motorA('L');
+      motorB('L');
+    }
+    // OFF -- SPACE
+    if(incomingByte == 32){
+      motorA('o');
+      motorB('o');
+    }
+
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+  }  
+}
